cd70cb3ee14ec8aab346b94229a9b1b43d60de53
Aspirant Sprain <aspirantsprain@gmail.com>
2025-06-23 18:02:43 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <ChessKnightIcon c

--- src/components/RotatingChessPiece.tsx
+++ src/components/RotatingChessPiece.tsx
@@ -1,14 +1,11 @@
 "use client";
-import { ChessKnightIcon } from './icons/ChessKnightIcon';
+import Lottie from "lottie-react";
+import animationData from "@/animations/chess.json";
 
 export function RotatingChessPiece() {
   return (
-    <div className="w-64 h-64 md:w-80 md:h-80 flex items-center justify-center [perspective:1000px]">
-      <div className="relative w-full h-full animate-rotate-y-slow [transform-style:preserve-3d]">
-        <div className="absolute inset-0 flex items-center justify-center text-primary/80">
-          <ChessKnightIcon className="w-48 h-48 md:w-64 md:h-64" />
-        </div>
-      </div>
+    <div className="w-64 h-64 md:w-80 md:h-80 flex items-center justify-center">
+      <Lottie animationData={animationData} loop={true} />
     </div>
   );
 }
