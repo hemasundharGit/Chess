e2cb6f478a191e57e52525267778681137e83e58
Aspirant Sprain <aspirantsprain@gmail.com>
2025-06-25 16:32:41 +0000

    i had provided the new aniamtion file in the path of /home/user/studio/p

--- src/components/RotatingChessPiece.tsx
+++ src/components/RotatingChessPiece.tsx
@@ -1,12 +1,11 @@
 "use client";
 
 import Lottie from "lottie-react";
-import animationData from "@/animations/chess-animation.json";
 
 export function RotatingChessPiece() {
   return (
     <div className="w-64 h-64 md:w-80 md:h-80 flex items-center justify-center">
-      <Lottie animationData={animationData} loop={true} />
+      <Lottie path="/animations/Animation - 1750868903965.json" loop={true} />
     </div>
   );
 }
