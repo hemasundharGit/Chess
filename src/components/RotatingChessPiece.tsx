aa8b9c49fed512dacf1f45ae1fcc32779363670a
Aspirant Sprain <aspirantsprain@gmail.com>
2025-06-25 06:04:54 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <div class="contai

--- src/components/RotatingChessPiece.tsx
+++ src/components/RotatingChessPiece.tsx
@@ -1,28 +1,10 @@
 "use client";
 
 import Lottie from "lottie-react";
-import React, { useEffect, useState } from "react";
+import React from "react";
+import animationData from "@/animations/chess.json";
 
 export function RotatingChessPiece() {
-  const [animationData, setAnimationData] = useState(null);
-
-  useEffect(() => {
-    fetch('/animations/chess.json')
-      .then((response) => response.json())
-      .then((data) => {
-        setAnimationData(data);
-      })
-      .catch(error => console.error('Error loading animation:', error));
-  }, []);
-
-  if (!animationData) {
-    return (
-      <div className="w-64 h-64 md:w-80 md:h-80 flex items-center justify-center">
-        <p>Loading Animation...</p>
-      </div>
-    );
-  }
-
   return (
     <div className="w-64 h-64 md:w-80 md:h-80 flex items-center justify-center">
         <Lottie animationData={animationData} loop={true} />
