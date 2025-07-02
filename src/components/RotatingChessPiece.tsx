9712de12340d47560e82af449aa0d760dcef8187
Aspirant Sprain <aspirantsprain@gmail.com>
2025-07-02 06:08:06 +0000

    The NextJS app server is having trouble starting. Please identify what c

--- src/components/RotatingChessPiece.tsx
+++ src/components/RotatingChessPiece.tsx
@@ -5,7 +5,7 @@ import Lottie from "lottie-react";
 export function RotatingChessPiece() {
   return (
     <div className="w-64 h-64 md:w-80 md:h-80 flex items-center justify-center">
-      <Lottie src="/Animation - 1750868903965.json" loop={true} />
+      <Lottie src="/animations/Animation - 1750868903965.json" loop={true} />
     </div>
   );
 }
