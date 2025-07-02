3e88a437edc00f0fad8504a31b398e24345bae7b
Aspirant Sprain <aspirantsprain@gmail.com>
2025-07-02 06:24:31 +0000

    unable to see the animation in the home page

--- src/components/RotatingChessPiece.tsx
+++ src/components/RotatingChessPiece.tsx
@@ -5,7 +5,7 @@ import Lottie from "lottie-react";
 export function RotatingChessPiece() {
   return (
     <div className="w-64 h-64 md:w-80 md:h-80 flex items-center justify-center">
-      <Lottie path="/animations/Animation - 1750868903965.json" loop={true} />
+      <Lottie path="/animations/Animation%20-%201750868903965.json" loop={true} />
     </div>
   );
 }
